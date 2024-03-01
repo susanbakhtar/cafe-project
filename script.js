d18273dcd973afa8507d11d5bbfafa2163d7e93a
susanbakhtar <148472264+susanbakhtar@users.noreply.github.com>
2024-03-01 18:39:41 +0330

    din't any version

---
+++ script.js
@@ -0,0 +1,9 @@
+VanillaTilt.init(document.querySelectorAll(".img-library-move-mouse"), {
+  max: 30,
+  speed: 400,
+  glare: true,
+  // for phone!
+  gyroscope: true,
+  "full-page-listening": true,
+  sensitivity: 5,
+});
